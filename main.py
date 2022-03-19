acde0adca478e40cca0098d6d7a43bec3a5d8842
Timo <66961498+nuokey@users.noreply.github.com>
2022-03-20 01:04:02 +0300

    Update

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+def index():
+    return render_template('index.html')
 
+if __name__ == '__main__':
+    app.run()

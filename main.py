542441571bd8db6d1393049ec793e9c82c3a352a
Timo <66961498+nuokey@users.noreply.github.com>
2022-03-30 10:35:39 +0300

    Update

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+
+@app.route('/ahah')
+def ahah():
+    return render_template('ahah.html')
+
 if __name__ == '__main__':
     app.run()
